a0e2c0d2c6c13a90023326815365fe15582cf824
xishengbo <xishengbo@baidu.com>
2023-07-18 08:42:57 +0800

    23-07-18 添加分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,15 +1,145 @@
-// index.js
-// const app = getApp()
+const image = 'https://res.app.ikea.cn/content/u/20220119/f99644d37a3641bab08a0f4806bdc2d2.jpg?x-oss-process=image//interlace,1/resize,w_200';
+const items = new Array(10).fill({ label: '标题' }, 0, 10);
+
 Page({
+    offsetTopList: [],
     data: {
-        haveCreateCollection: false
+        sideBarIndex: 0,
+        scrollTop: 0,
+        categories: [
+            {
+                label: '门窗',
+                title: '门窗淋浴房',
+                items: [
+                    {label: '​平移窗封阳台', image},
+                    {label: '漂移窗封阳台', image},
+                    {label: '​内开窗封阳台', image},
+                    {label: '​淋浴房', image},
+                    {label: '​卫生间玻璃移门', image},
+                    {label: '​折叠门', image}
+                ]
+            },
+            {
+                label: '硬装',
+                title: '硬装局改',
+                items: [
+                    {label: '​通铺地板/瓷砖', image},
+                    {label: '​阳台墙砖', image},
+                    {label: '​室内拆墙与改造', image},
+                    {label: '​吊顶扩宽与定制', image},
+                    {label: '​移电位', image},
+                    {label: '​刷漆服务', image}
+                ]
+            },
+            {
+                label: '回收​',
+                title: '物品回收​',
+                items: [
+                    {label: '​木板回收', image},
+                    {label: '​家电回收', image},
+                    {label: '​阳台移门回收', image}
+                ]
+            },
+            {
+                label: '​橱柜',
+                title: '​橱柜/阳台柜',
+                items: [
+                    {label: '​橱柜换台面', image},
+                    {label: '​橱柜拉篮升级', image},
+                    {label: '​橱柜全升级', image},
+                    {label: '​阳台柜定制', image},
+                    {label: '​飘窗柜定制', image}
+                ]
+            },
+            {
+                label: '​全屋定制',
+                title: '​全屋定制',
+                items: [
+                    {label: '​索菲亚', image},
+                    {label: '​欧派', image},
+                    {label: '​我乐', image},
+                    {label: '​工厂定制', image}
+                ]
+            },
+            {
+                label: '​纱窗',
+                title: '​纱窗定制',
+                items: [
+                    {label: '​高透网纱窗', image},
+                    {label: '​隐形纱窗', image}
+                ]
+            },
+            {
+                label: '​安装',
+                title: '​安装服务',
+                items: [
+                    {label: '​灯具安装', image},
+                    {label: '​家电安装', image},
+                    {label: '​网络安装', image},
+                    {label: '​其他安装', image}
+                ]
+            },
+            {
+                label: '窗帘',
+                title: '窗帘',
+                items: [
+                    {label: '​全屋窗帘', image},
+                    {label: '​百叶帘', image}
+                ]
+            },
+            {
+                label: '美缝​',
+                title: '全屋美缝​',
+                items: [
+                    {label: '​德高美缝', image},
+                    {label: '​皇氏工匠美缝', image},
+                    {label: '​卓高美缝', image}
+                ]
+            },
+            {
+                label: '​其他',
+                title: '​其他项目',
+                items: [
+                    {label: '​扫地机器人位置改造', image},
+                    {label: '​反坎贴砖', image},
+                    {label: '​改插座', image},
+                    {label: '​未列项目可直接咨询', image}
+                ]
+            }
+        ]
     },
     onLoad() {
-        console.log('this is onLoad')
+        // 载入后动态就算每个分类标题的top 用于后续滚动定位
+        const query = wx.createSelectorQuery().in(this);
+        const {sideBarIndex} = this.data;
+        query.selectAll('.title')
+            .boundingClientRect((rects) => {
+                console.log(rects, 'rects');
+                this.offsetTopList = rects.map((item) => item.top);
+                this.setData({ scrollTop: rects[sideBarIndex].top });
+            })
+        .exec();
     },
-    jumpTo(event) {
-        wx.navigateTo({
-            url: `/pages/${event.target.dataset.path}/index`,
+    // 侧边栏点击响应
+    onSideBarChange(e) {
+        const {value} = e.detail;
+        this.setData({
+            sideBarIndex: value,
+            scrollTop: this.offsetTopList[value]
         });
+    },
+    // 滑动的时候切换侧边栏activeIdx
+    onScroll(e) {
+        const {scrollTop} = e.detail;
+        // 下一个标题与顶部的距离
+        const threshold = 50;
+        if (scrollTop < threshold) {
+            this.setData({ sideBarIndex: 0 });
+            return;
+        }
+        const index = (this.offsetTopList || []).findIndex((top) => top > scrollTop && top - scrollTop <= threshold);
+        if (index > -1) {
+            this.setData({ sideBarIndex: index });
+        }
     }
 });
